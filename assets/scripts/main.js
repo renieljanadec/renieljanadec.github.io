ac705603b2577b8f0b6d47562e584c756ab4d81c
renieljanadec <decnepomuceno@gmail.com>
2018-07-11 14:04:08 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var open = document.getElementById('burger-btn');
+    var close = document.getElementById('close-btn');
+
+    //toggle side navigation
+    open.addEventListener('click', function() {
+
+        document.getElementById('side-navigation').style.width = '100%';
+    });
+
+    //close side navigation
+    close.addEventListener('click', function() {
+        document.getElementById('side-navigation').style.width = '0';
+    });
+
+    //close side navigation when a link is clicked
+    function close() {
+        document.getElementById('side-navigation').style.width = '0';
+    }
+});
+
+
+/* JQUERY WAY */
+/*$(document).ready(function(){
+
+    //toggle nav
+    $('#burger-btn').click(function(){
+        $('#side-navigation').css("width", "100%");
+    });
+
+    //close nav
+    $('#close-btn').click(function(){
+        $('#side-navigation').css("width", "0");
+    });
+
+});*/
